89b0da5ab4585ac64645a427bdfe8df4eb5bec72
Ambinintsoa-Fifaliana <167977613+Ambinintsoa-Fifaliana@users.noreply.github.com>
2024-09-07 12:45:23 +0300

    Add files via upload

---
+++ calcul.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int verify_cutting_number(int bits_reseau , int verify_ip_br);
+int nombre_sous_reseau(int decoupage);
+int nombre_bits_hotes( int verify_ip_br , int decoupage);
+int byte(int nbr_bits_hotes);
+int masque_sous_reseau(int decoupage);
